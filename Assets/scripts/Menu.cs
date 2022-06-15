95d1ff45b864e9f52c0aa8aecd163c1091f5d950
otottkovi2 <otottkovi@hotmail.com>
2022-06-15 21:08:47 +0200

    bekötött menü gombok

--- Assets/scripts/Menu.cs
+++ Assets/scripts/Menu.cs
@@ -7,8 +7,11 @@ using UnityEngine.UIElements;
 public class Menu : MonoBehaviour
 {
 
-    public GameObject loading;  //the loading screen
-    private VisualElement UIroot;
+    //public GameObject loading;  //the loading screen, not needed yet
+    private VisualElement uiRoot;
+    private TemplateContainer noSaveError;
+
+
 
     // this has all the things for the menu like button events, menu mechanics and more...
     public void NewGame()
@@ -20,50 +23,51 @@ public class Menu : MonoBehaviour
         System.GC.Collect();
         //loading the game
         SceneManager.LoadScene(1);
-        loading.SetActive(true);    //show loading screen
-
+        
     }
 
     public void Continue()
     {
+        Debug.Log("continue clicked");
         //checking for save file, showing error if there isn't one, greying out the button would be a better solution though
         if (!PlayerPrefs.HasKey("game_progress"))
         {
-            
+            noSaveError.RemoveFromClassList("hidden");
 
         }
         else
         {
+            System.GC.Collect();
             SceneManager.LoadScene(1);
         }
     }
 
 
     //shows the options menu
-    public void Options(GameObject settingsPanel)
-        {
-            settingsPanel.SetActive(true);
-        }
+    public void Options()
+    {
+            
+    }
 
 
-    //presses ok the whatever it is assigned to
+    //presses ok and closes the error window
     // ReSharper disable once InconsistentNaming
-    public void ok(GameObject panel)
+    public void ok()
     {
-        panel.SetActive(false);
+       noSaveError.AddToClassList("hidden");
     }
 
-    //wrapper for GameHelper.Quit()
-    public void Exit()
-    {
-        GameHelper.Quit();
-    }
 
     private void Start()
     {
         PlayerPrefs.DeleteAll(); //This deletes the player's progress, for testing only!
-        UIroot = GetComponent<UIDocument>().rootVisualElement;
-        UIroot.Q<Button>("continue").clicked += Continue;
+        uiRoot = GetComponent<UIDocument>().rootVisualElement;
+        noSaveError = uiRoot.Q<TemplateContainer>("NoSaveError");
+        uiRoot.Q<Button>("continue").clicked += Continue;
+        uiRoot.Q<Button>("ok").clicked += ok;
+        uiRoot.Q<Button>("newgame").clicked += NewGame;
+        uiRoot.Q<Button>("options").clicked += Options;
+        uiRoot.Q<Button>("exit").clicked += GameHelper.Quit;
     }
 
 }
