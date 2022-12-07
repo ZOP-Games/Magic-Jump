1b428f2263aacfb552eebb8625de9f4dd3b181fd
otottkovi2 <otottkovi@hotmail.com>
2022-12-07 21:22:47 +0100

    added proper docs for player

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -10,13 +10,31 @@ using UnityEngine.Serialization;
 public class Player : Entity
 {
     // this is for things unique to the player (controls, spells, etc.)
+    /// <summary>
+    /// The main <see cref="Player"></see> Instance.
+    /// </summary>
     public static Player Instance { get; private set; }
 
     //public references to some objects in the scene
-    public PlayerInput PInput { get; private set; } //playerInput component
+    /// <summary>
+    /// The player's <see cref="PlayerInput"/> component.
+    /// </summary>
+    public PlayerInput PInput { get; private set; }
+    /// <summary>
+    /// The active <see cref="MenuController"></see> instance.
+    /// </summary>
     private MenuController menus;
+    /// <summary>
+    /// The active <see cref="SpellManager"/> instance.
+    /// </summary>
     private readonly SpellManager spells = SpellManager.Instance;
+    /// <summary>
+    /// The <see cref="SpellScreen"/> in use.
+    /// </summary>
     private SpellScreen spellScreen;
+    /// <summary>
+    /// The "level up!" text object.
+    /// </summary>
     [FormerlySerializedAs("LevelUpText")] [SerializeField] private TextMeshProUGUI levelUpText;
 
     //setting Entity properties, for more info -> see Entity
@@ -25,36 +43,96 @@ public class Player : Entity
     protected override Vector3 AtkSpherePos => new(0, 1, 0.5f);
     protected override int AtkSphereRadius => 1;
 
-    private bool mozog; //bool for checking if the player is moving or not
-    private Vector2 mPos; //Vector2 containing joystck input data
-    private bool grounded; //bool for checking if the player is on ground or not
-    private bool running; //bool for checking if the player is running or not
-    private int moveSpeedId; //id of the moveSpeed parameter, for controlling animation speed from script
-    private Transform tf; //the player's Transform
-    private CinemachineVirtualCamera vCam; //the main virtual camera in the scene
+    /// <summary>
+    /// Used for checking if the player is moving or not
+    /// </summary>
+    private bool mozog;
+    /// <summary>
+    /// Contains joystck input data.
+    /// </summary>
+    private Vector2 mPos; 
+    /// <summary>
+    /// Used for checking if the player is on ground or not
+    /// </summary>
+    private bool grounded; 
+    /// <summary>
+    /// Used for checking if the player is running or not
+    /// </summary>
+    private bool running;
+    /// <summary>
+    /// id of the moveSpeed parameter, for controlling animation speed from script
+    /// </summary>
+    private static int MoveSpeedId => Animator.StringToHash("moveSpeed");
+    /// <summary>
+    /// the player's <see cref="Transform"/>.
+    /// </summary>
+    private Transform tf; 
+    /// <summary>
+    /// the main <see cref="CinemachineVirtualCamera"/> in the scene
+    /// </summary>
+    private CinemachineVirtualCamera vCam;
 
+    /// <summary>
+    /// The player's XP.
+    /// </summary>
     public int Xp { get; private set; }
+    /// <summary>
+    /// The player level.
+    /// </summary>
     public byte Lvl { get; private set; }
+    /// <summary>
+    /// The amount of XP the player needs to level up.
+    /// </summary>
     public int XpThreshold { get; private set; } = 10;
 
     //some constants to make code readable + adjustable
+    /// <summary>
+    /// The force the player's <see cref="Rigidbody"/> pushes it up to jump. You can change its value to adjust jump height.
+    /// </summary>
     private const int JumpForce = 400;
+    /// <summary>
+    /// The force the player's <see cref="Rigidbody"/> pushes it to dodge. You can change its value to adjust dodge distance.
+    /// </summary>
     private const int DodgePower = 23;
+    /// <summary>
+    /// The amount of seconds the game waits to look at the objective.
+    /// </summary>
     private const int LookTimeout = 3;
+    /// <summary>
+    /// The amount of damping on the camera while walking. You can change its value to adjust how far the camera zooms out while walking.
+    /// </summary>
     private const int WalkDamping = 5;
+    /// <summary>
+    /// The amount of damping on the camera while running. You can change its value to adjust how far the camera zooms out while running.
+    /// </summary>
+    /// <remarks>It generally should be smaller than <see cref="WalkDamping"/>. </remarks>
     private const int RunDamping = 2;
+    /// <summary>
+    /// Constraint sum of the <see cref="Rigidbody"/> component. See <see href="https://docs.unity3d.com/ScriptReference/Rigidbody-constraints.html"/> for details.
+    /// </summary>
+    /// <remarks>Don't change this unless physics don't move as they should.</remarks>
     private const int Constraints = 80;
+    /// <summary>
+    /// The start XP threshold which then gets scaled to the player's <see cref="Lvl"/>.
+    /// </summary>
     private const int DefaultThreshold = 10;
+    /// <summary>
+    /// The multiplier the XP threshold increases with each level. You can change its value to adjust how much more difficult each level gets.
+    /// </summary>
     private const float ThresholdMultiplier = 1.2f;
 
     //input event handlers
+    /// <summary>
+    /// Gets the player's movement input and saves that data to <see cref="mPos"/>.
+    /// </summary>
+    /// <param name="context">The input data.</param>
     public void Move(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
             mPos = context.ReadValue<Vector2>(); //storing input data
             anim.SetBool(MovingPmHash, true); //playing the move animation
-            anim.SetFloat(moveSpeedId, 1); //setting aniation playback speed to 1
+            anim.SetFloat(MoveSpeedId, 1); //setting aniation playback speed to 1
             mozog = true; //telling the code in FixedUpdate() that the player is moving
         }
 
@@ -64,14 +142,20 @@ public class Player : Entity
         anim.SetBool(MovingPmHash, false);
         mPos = Vector2.zero;
     }
-
+    /// <summary>
+    /// Event handler for jumping.
+    /// </summary>
+    /// <param name="context">The input data.</param>
     public void Jump(InputAction.CallbackContext context)
     {
         if (!context.performed || !grounded) return;
         rb.AddForce(0, JumpForce, 0); //jump
         //Debug.Log("Jumping, velocity: " + rb.velocity.y); 
     }
-
+    /// <summary>
+    /// Event handler for standard (light) attacking.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void LightAttack(InputAction.CallbackContext context)
     {
         if (context.performed)
@@ -79,7 +163,10 @@ public class Player : Entity
             Attack(); //see Entity.Attack()
         }
     }
-
+    /// <summary>
+    /// Event handler for second (heavy) attacking.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void HeavyAttack(InputAction.CallbackContext context)
     {
         //we don't know what to do with this yet :/
@@ -88,7 +175,10 @@ public class Player : Entity
             Debug.Log("Heavy Attack");
         }
     }
-
+    /// <summary>
+    /// Event handler for dodging.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void Dodge(InputAction.CallbackContext context)
     {
         if (context.performed)
@@ -96,23 +186,29 @@ public class Player : Entity
             rb.AddForce(rb.velocity.x + DodgePower, 0, 0); //pushing player to the side (idk if we still need this tbh)
         }
     }
-
+    /// <summary>
+    /// Event handler for running.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void Run(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
             //tell the code in FixedUpdate() we're running
             running = true;
-            anim.SetFloat(moveSpeedId, 2); //set "running" animation (speeding up walk animation)
+            anim.SetFloat(MoveSpeedId, 2); //set "running" animation (speeding up walk animation)
             vCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ZDamping = RunDamping;
         }
 
         if (!context.canceled) return;
         running = false;
-        anim.SetFloat(moveSpeedId, 1);
+        anim.SetFloat(MoveSpeedId, 1);
         vCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ZDamping = WalkDamping;
     }
-
+    /// <summary>
+    /// Event handler for using spells.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void UseSpell(InputAction.CallbackContext context)
     {
         if (!context.performed) return;
@@ -121,14 +217,20 @@ public class Player : Entity
         spell.Use(GetEntities(AtkSpherePos,AtkSphereRadius));
         Debug.Log("Use Spell: " + spell);
     }
-
+    /// <summary>
+    /// Event handler for changing spells.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void ChangeSpell(InputAction.CallbackContext context)
     {
         if (!context.performed) return;
         spellScreen.Open();
         Debug.Log("Change Spell to: " + spells.SelectedSpell);
     }
-
+    /// <summary>
+    /// Event handler for pausing.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void Pause(InputAction.CallbackContext context)
     {
         //Debug.Log("menu is " + context.phase);
@@ -137,13 +239,18 @@ public class Player : Entity
         menus.OpenPause();
         Debug.Log("Paused");
     }
-
+    /// <summary>
+    /// Closes the active menu. Used for when a <see cref="UnityEngine.UI.Button"/> is pressed.
+    /// </summary>
     public void CloseMenu()
     {
         menus.CloseActive();
         Debug.Log("Closed active menu");
     }
-
+    /// <summary>
+    /// Closes the active menu. Used for pressing the 'cancel' <see cref="UnityEngine.InputSystem.InputAction"/>.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void CloseMenu(InputAction.CallbackContext context)
     {
         Debug.Log("unpause is " + context.phase);
@@ -151,7 +258,10 @@ public class Player : Entity
         menus.CloseActive();
         Debug.Log("Closed active menu");
     }
-
+    /// <summary>
+    /// Event handler for looking at the objective.
+    /// </summary>
+    /// <param name="context"><inheritdoc cref="Jump"/></param>
     public void ShowObjective(InputAction.CallbackContext context)
     {
         //again, not sure if we need this but it's here anyway
@@ -160,32 +270,47 @@ public class Player : Entity
         Debug.Log("Show Objective");
         Invoke(nameof(DoneLooking), LookTimeout); //after 3 seconds, return to normal camera view
     }
-
+    /// <summary>
+    /// Looks back at the player.
+    /// </summary>
+    /// <remarks>It's used for calling it in Invoke().</remarks>
     private void DoneLooking()
     {
         Debug.Log("Camera looks back at player");
-        //Camera.current.GetComponent<CinemachineVirtualCamera>().LookAt = tf;
+        //vCam.LookAt = tf;
     }
 
-
+    /// <summary>
+    /// Kills the player.
+    /// </summary>
+    /// <remarks>[overriden from <see cref="Entity.Die"/>]</remarks>
     public override void Die()
     {
         Debug.Log("player died :(");
         gameObject.SetActive(false);
     }
-
+    /// <summary>
+    /// Stuns the player.
+    /// </summary>
+    /// <remarks>[overriden from <see cref="Entity.Stun"/>]</remarks>
     public override void Stun()
     {
         base.Stun();
         PInput.DeactivateInput();
     }
-
+    /// <summary>
+    /// Cancels the stun effect.
+    /// </summary>
+    /// <remarks>[overriden from <see cref="Entity.UnStun"/>]</remarks>
     protected override void UnStun()
     {
         PInput.ActivateInput();
         base.UnStun();
     }
-
+    /// <summary>
+    /// Adds XP to the player, and levels it up if necesarry.
+    /// </summary>
+    /// <param name="amount">The amount of XP given to the player.</param>
     public void AddXp(int amount)
     {
         Xp += amount;
@@ -198,14 +323,11 @@ public class Player : Entity
     }
 
     
-
-    //jumping thing: checks for being on the ground before jumping
     protected void OnCollisionStay(Collision collision)
     {
         if (collision.collider is TerrainCollider && !grounded) grounded = true;    //if the player is touching the ground, they can jump
 
     }
-
     private void OnCollisionExit(Collision collision)
     {
         if (collision.collider is TerrainCollider && grounded) grounded = false;    //as soon as the player leaves the ground, they can't jump
@@ -225,7 +347,6 @@ public class Player : Entity
     //Start() runs once when the object is enabled, lots of early game setup goes here
     private void Start()
     {
-        moveSpeedId = Animator.StringToHash("moveSpeed"); //setting moveSpeedId
         PInput = GetComponent<PlayerInput>(); //setting PlayerInput
         //PlayerInput setup inside
 
@@ -249,7 +370,7 @@ public class Player : Entity
 
             #endregion
         tag = "Player"; //setting a player, helps w/ identification
-        rb = GetComponent<Rigidbody>(); //getting Rigidbody and Animator and Trasnform
+        rb = GetComponent<Rigidbody>(); //getting Rigidbody and Animator and Trasnform and MenuController and SpellScreen
         //Rigibody setup inside
         #region rbSetup
 
@@ -259,20 +380,20 @@ public class Player : Entity
         
 
             #endregion
-        Instance = GameObject.Find("player").GetComponent<Player>();
-        anim = GetComponent<Animator>();
+            anim = GetComponent<Animator>();
         tf = transform;
+        menus = MenuController.Controller;
+        spellScreen = FindObjectOfType<SpellScreen>(true);
+        Instance = GameObject.Find("player").GetComponent<Player>();    //setting Instance
         vCam = CinemachineCore.Instance.GetVirtualCamera(0) as CinemachineVirtualCamera;   //getting virtual camera and setting damping to default value
         if (vCam != null) vCam.GetCinemachineComponent<CinemachineFramingTransposer>().m_ZDamping = WalkDamping;
         hpText = GetComponentInChildren<TextMeshPro>(); //getting hp text and setting to default value
         hpText.SetText("HP: 100");
-        PlayerPrefs.SetInt("PlayerXp",12);
+        PlayerPrefs.SetInt("PlayerXp",12);  //setting XP (for dev purposes)
         PlayerPrefs.SetInt("PlayerLvl",1);
-        Xp = PlayerPrefs.GetInt("PlayerXp");
+        Xp = PlayerPrefs.GetInt("PlayerXp");    //getting XP and Level, then calculating the current XP threshold
         Lvl = (byte)PlayerPrefs.GetInt("PlayerLvl");
         XpThreshold = (int)(DefaultThreshold*ThresholdMultiplier * Lvl);
-        menus = MenuController.Controller;
-        spellScreen = FindObjectOfType<SpellScreen>(true);
     }
 }
 
