52d09207022657d765876ac4281a1edaa0709cbb
otottkovi2 <otottkovi@hotmail.com>
2023-04-25 21:22:55 +0200

    volume settings work

--- Assets/scripts/UI/Layouts/AudioSettingsLayout.cs
+++ Assets/scripts/UI/Layouts/AudioSettingsLayout.cs
@@ -1,14 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
 
 namespace GameExtensions.UI.Layouts
 {
     public class AudioSettingsLayout : ScreenLayout
     {
+       [SerializeField]private AudioMixer masterMixer;
+
         private new void OnEnable()
         {
             base.OnEnable();
+        }
+
+        private new void Start()
+        {
+            base.Start();
+        }
+
+        public void ChangeMasterVolume(float amount)
+        {
+            masterMixer.SetFloat("MasterVolume", amount);
+        }
+        
+        public void ChangeBackgroundVolume(float amount)
+        {
+            masterMixer.SetFloat("BgVolume", amount);
+        }
+        
+        public void ChangeSfxVolume(float amount)
+        {
+            masterMixer.SetFloat("SFXVolume", amount);
+        }
+        
+        public void ChangeSpeechVolume(float amount)
+        {
+            masterMixer.SetFloat("SpeechVolume", amount);
         }
     }
 }
--- Assets/scripts/UI/Layouts/ScreenLayout.cs
+++ Assets/scripts/UI/Layouts/ScreenLayout.cs
@@ -6,9 +6,10 @@ using UnityEngine.UI;
 
 namespace GameExtensions.UI.Layouts
 {
-    public class ScreenLayout : UIComponent
+    public abstract class ScreenLayout : UIComponent
     {
         [SerializeField] private GameObject firstObj;
+        protected Selectable[] options;
 
         protected virtual void OnEnable()
         {
@@ -20,11 +21,11 @@ namespace GameExtensions.UI.Layouts
             ES.SetSelectedGameObject(firstObj); //todo: fix object selection
         }
 
-        private void Start()
+        protected virtual void Start()
         {
             UnityEngine.Debug.Assert(ES.currentInputModule is InputSystemUIInputModule,
                 "Current Input Module is not of type InputSystemUIInputModule.");
-            var input = ES.currentInputModule as InputSystemUIInputModule;
+            options = GetComponentsInChildren<Selectable>().Where(s => s is not Scrollbar).ToArray();
         }
     }
 }
